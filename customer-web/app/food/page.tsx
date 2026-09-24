628faf0136ee7ef80a303f662494bae92c81bcea
Jesuefe <101691268+Jesuefe@users.noreply.github.com>
2026-09-24 11:42:08 +0200

    fix: localize food page

--- customer-web/app/food/page.tsx
+++ customer-web/app/food/page.tsx
@@ -1,4 +1,4 @@
 import ShopPage from '../../components/ShopPage';
 export default function FoodPage() {
-  return <ShopPage category="FOOD" title="Food & Drinks" emptyMessage="No restaurants available yet." />;
+  return <ShopPage category="FOOD" title={t('Food & Drinks')} emptyMessage={t('No restaurants available yet.')} />;
 }
