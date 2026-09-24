1434de98f05690498893176a39d04d4268808e54
Jesuefe <101691268+Jesuefe@users.noreply.github.com>
2026-09-24 10:37:51 +0200

    i18n: wire customer UI text to local language dictionary

--- customer-web/app/gifts/page.tsx
+++ customer-web/app/gifts/page.tsx
@@ -1,4 +1,5 @@
 import ShopPage from '../../components/ShopPage';
 export default function GiftsPage() {
+  const { t } = useLang();
   return <ShopPage category="GIFTS" title="Gifts & Flowers" emptyMessage="No gift shops available yet." />;
 }
