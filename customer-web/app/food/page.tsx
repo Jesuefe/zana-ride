6f7c13e0300e17f44a005c5cd83855ad111d4a8b
Jesuefe <101691268+Jesuefe@users.noreply.github.com>
2026-09-24 10:37:45 +0200

    i18n: wire customer UI text to local language dictionary

--- customer-web/app/food/page.tsx
+++ customer-web/app/food/page.tsx
@@ -1,4 +1,5 @@
 import ShopPage from '../../components/ShopPage';
 export default function FoodPage() {
+  const { t } = useLang();
   return <ShopPage category="FOOD" title="Food & Drinks" emptyMessage="No restaurants available yet." />;
 }
