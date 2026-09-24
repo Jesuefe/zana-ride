3f786370d828cf464aded67fe85d22790290cae7
Jesuefe <101691268+Jesuefe@users.noreply.github.com>
2026-09-24 11:42:15 +0200

    fix: wire food page localization

--- customer-web/app/food/page.tsx
+++ customer-web/app/food/page.tsx
@@ -1,4 +1,8 @@
+'use client';
+
+import { useLang } from '../../lib/LangContext';
 import ShopPage from '../../components/ShopPage';
 export default function FoodPage() {
+  const { t } = useLang();
   return <ShopPage category="FOOD" title={t('Food & Drinks')} emptyMessage={t('No restaurants available yet.')} />;
 }
