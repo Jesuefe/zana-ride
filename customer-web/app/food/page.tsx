d181e6f5595ed31291751f241580854e181ea5ab
Jesuefe <101691268+Jesuefe@users.noreply.github.com>
2026-09-24 10:38:21 +0200

    i18n: remove unused language hook

--- customer-web/app/food/page.tsx
+++ customer-web/app/food/page.tsx
@@ -1,5 +1,4 @@
 import ShopPage from '../../components/ShopPage';
 export default function FoodPage() {
-  const { t } = useLang();
   return <ShopPage category="FOOD" title="Food & Drinks" emptyMessage="No restaurants available yet." />;
 }
